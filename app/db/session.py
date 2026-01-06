9f3b80b103fccafceec86a8d5082bd5382d0efd6
Euan Jones <euanajones@gmail.com>
2026-01-06 18:12:18 +0000

    Created some example User model instances

--- app/db/session.py
+++ app/db/session.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, Session, create_engine
 from config import settings
 from models import User, Organisation, OrganisationMembership, Plan
 
@@ -7,5 +7,22 @@ engine = create_engine(settings.DATABASE_URL, echo=True)
 def create_tables():
     SQLModel.metadata.create_all(engine)
 
-if __name__ == "__main__":
+def create_users():
+    user1 = User(email="tstark@email.com", password_hash="password1")
+    user2 = User(email="bbanner@email.com", password_hash="password2")
+    user3 = User(email="pparker@email.com", password_hash="password3")
+
+    session = Session(engine)
+
+    session.add(user1)
+    session.add(user2)
+    session.add(user3)
+
+    session.commit()
+
+def main():
     create_tables()
+    create_users()
+
+if __name__ == "__main__":
+    main()
