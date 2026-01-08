e31b37c9c78b9545bbd7ed4a8b18dbadc0c6ce3e
Euan Jones <euanajones@gmail.com>
2026-01-08 19:35:58 +0000

    Recreated User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,8 @@
-from sqlmodel import Field, SQLModel
-from datetime import datetime
+from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     first_name: str
     last_name: str
     email: str
-    password_hash: str
-    date_created: datetime = Field(default_factory=datetime.now)
+    password: str
