477740254efd783ac31381ba59a9b7e05333e55c
Euan Jones <euanajones@gmail.com>
2026-01-08 19:42:41 +0000

    Recreated Subscription model

--- app/models/subscription.py
+++ app/models/subscription.py
@@ -1,15 +1,9 @@
-from sqlmodel import Field, SQLModel, Relationship
+from sqlmodel import SQLModel, Field
 from datetime import datetime
-
-from .organisation import Organisation
-from .plan import Plan
 
 class Subscription(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    organisation_id: int = Field(foreign_key="organisation.id")
-    plan_id: int = Field(foreign_key="plan.id")
-    status: str
-    end_date: datetime | None = None
-
-    organisation: "Organisation" = Relationship(back_populates="organisation")
-    plan: "Plan" = Relationship(back_populates="plan")
+    user_id: int | None = Field(default=None, foreign_key="user.id")
+    plan_id: int | None = Field(default=None, foreign_key="plan.id")
+    start_date: datetime
+    end_date: datetime
