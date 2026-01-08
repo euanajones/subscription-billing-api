79ba953e7b47824b14bbcbb54bb3d05561bc5656
Euan Jones <euanajones@gmail.com>
2026-01-08 19:44:28 +0000

    Recreated Organisation model and updated init

--- app/models/organisation.py
+++ app/models/organisation.py
@@ -1,7 +1,7 @@
-from sqlmodel import Field, SQLModel
-from datetime import datetime
+from sqlmodel import SQLModel, Field
 
 class Organisation(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    date_created: datetime = Field(default_factory=datetime.now)
+
+    owner_id: int | None = Field(default=None, foreign_key="user.id")
--- app/models/subscription.py
+++ app/models/subscription.py
@@ -3,7 +3,9 @@ from datetime import datetime
 
 class Subscription(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+
     user_id: int | None = Field(default=None, foreign_key="user.id")
     plan_id: int | None = Field(default=None, foreign_key="plan.id")
+    
     start_date: datetime
     end_date: datetime
