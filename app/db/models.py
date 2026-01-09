024f9ecb042db3401a603e414e1d8ca89ff26b43
Euan Jones <euanajones@gmail.com>
2026-01-09 21:03:52 +0000

    Added 1:M relationships between users, plans and subscriptions

--- app/db/models.py
+++ app/db/models.py
@@ -8,15 +8,21 @@ class User(SQLModel, table=True):
     email: str
     password: str
 
+    # 1 User : N Organisations
     organisations: list["Organisation"] = Relationship(back_populates="owner")
+
+    # 1 User : N Subscriptions
+    subscriptions: list["Subscription"] = Relationship(back_populates="user", cascade_delete=True)
 
 class Organisation(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
 
-    owner_id: int = Field(default=None, foreign_key="user.id", ondelete="SET NULL")
+    owner_id: int | None = Field(default=None, foreign_key="user.id", ondelete="SET NULL")
+    # 1 Organisation : 1 User
     owner: User = Relationship(back_populates="organisations")
 
+    # 1 Organisation : N Plans
     plans: list["Plan"] = Relationship(back_populates="organisation_owner", cascade_delete=True)
 
 class Plan(SQLModel, table=True):
@@ -26,13 +32,22 @@ class Plan(SQLModel, table=True):
     interval_days: int
     
     org_id: int | None = Field(default=None, foreign_key="organisation.id", ondelete="CASCADE")
+    # 1 Plan : 1 Organisation
     organisation_owner: Organisation = Relationship(back_populates="plans")
+
+    # 1 Plan : N Subscriptions
+    subscriptions: list["Subscription"] = Relationship(back_populates="plan", cascade_delete=True)
 
 class Subscription(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
-    user_id: int | None = Field(default=None, foreign_key="user.id")
-    plan_id: int | None = Field(default=None, foreign_key="plan.id")
+    user_id: int | None = Field(default=None, foreign_key="user.id", ondelete="CASCADE")
+    # 1 Subscription : 1 User
+    user: User = Relationship(back_populates="subscriptions")
+
+    plan_id: int | None = Field(default=None, foreign_key="plan.id", ondelete="CASCADE")
+    # 1 Subscription : 1 Plan
+    plan: Plan = Relationship(back_populates="subscriptions")
 
     start_date: datetime
     end_date: datetime
