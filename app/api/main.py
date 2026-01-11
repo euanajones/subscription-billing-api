c5c153100b852c59753491f98f40001dac01208a
Euan Jones <euanajones@gmail.com>
2026-01-11 15:36:46 +0000

    Added fastapi dependencies for sessions

--- app/api/main.py
+++ app/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query, Depends
 from sqlmodel import SQLModel, Session, create_engine, select
 from app.settings.config import settings
 from app.db.models import User, UserCreate, UserPublic, UserUpdate
@@ -7,6 +7,10 @@ app = FastAPI()
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(settings.DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
@@ -23,63 +27,63 @@ def on_startup():
     create_tables()
 
 @app.post("/users/add", response_model=UserPublic)
-def create_user(user: UserCreate):
+def create_user(*, session: Session = Depends(get_session), user: UserCreate):
     hashed_password = hash_password(user.password)
-    with Session(engine) as session:
-        extra_data = {"hashed_password": hashed_password}
-        db_user = User.model_validate(user, update=extra_data)
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
-        
-        return db_user
+    extra_data = {"hashed_password": hashed_password}
+    db_user = User.model_validate(user, update=extra_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    
+    return db_user
     
 @app.get("/users", response_model=list[UserPublic])
-def get_users(offset: int = 0, limit: int = Query(default=30, le=100)):
-    with Session(engine) as session:
-        users = session.exec(select(User)).all()
-        return users
+def get_users(
+    *, 
+    session: Session = Depends(get_session), 
+    offset: int = 0, 
+    limit: int = Query(default=30, le=100)
+):
+    users = session.exec(select(User)).all()
+    return users
     
 @app.get("/users/{user_id}", response_model=UserPublic)
-def get_user_by_id(user_id: int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
-        
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found.")
-        return user
+def get_user_by_id(*, session: Session = Depends(get_session) ,user_id: int):
+    user = session.get(User, user_id)
+    
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    return user
     
 @app.patch("/users/{user_id}", response_model=UserPublic)
-def update_user(user_id: int, user: UserUpdate):
-    with Session(engine) as session:
-        db_user = session.get(User, user_id)
-        
-        if not db_user:
-            raise HTTPException(status_code=404, detail="User not found.")
-        
-        user_data = user.model_dump(exclude_unset=True)
+def update_user(*, session: Session = Depends(get_session) ,user_id: int, user: UserUpdate):
+    db_user = session.get(User, user_id)
+    
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    
+    user_data = user.model_dump(exclude_unset=True)
 
-        extra_data = {}
-        if "password" in user_data:
-            password = user_data["password"]
-            hashed_password = hash_password(password)
-            extra_data["hashed_password"] = hashed_password
+    extra_data = {}
+    if "password" in user_data:
+        password = user_data["password"]
+        hashed_password = hash_password(password)
+        extra_data["hashed_password"] = hashed_password
 
-        db_user.sqlmodel_update(user_data, update=extra_data)
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    db_user.sqlmodel_update(user_data, update=extra_data)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
 
-        return db_user
+    return db_user
 
 @app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    with Session(engine) as session:
-        user = session.get(User, user_id)
+def delete_user_by_id(*, session: Session = Depends(get_session) ,user_id: int):
+    user = session.get(User, user_id)
 
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found.")
-        
-        session.delete(user)
-        session.commit()
-        return {"User: {user_id} - Deleted": True}
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    
+    session.delete(user)
+    session.commit()
+    return {"User: {user_id} - Deleted": True}
