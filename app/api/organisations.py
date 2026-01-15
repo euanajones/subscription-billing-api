8db8f9ae2d35fed2e784ef8f79a524a61bb9006c
Euan Jones <euanajones@gmail.com>
2026-01-15 01:21:06 +0000

    Moved organisation endpoints into seperate file to improve readability and maintainability

---
+++ app/api/organisations.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, HTTPException, Query, Depends
+from sqlmodel import SQLModel, Session, create_engine, select
+from app.db.models import User, UserPublic, Organisation, OrganisationPublic, OrganisationUpdate, OrganisationPublicWithOwner, OrganisationCreate
+from main import app, get_session, hash_password
+
+@app.post("/organisations/create", response_model=OrganisationPublic)
+def create_organisation(
+    *, 
+    session: Session = Depends(get_session), 
+    organisation: OrganisationCreate
+    ):
+    db_organisation = Organisation.model_validate(organisation)
+    session.add(db_organisation)
+    session.commit()
+    session.refresh(db_organisation)
+    return db_organisation
+
+@app.get("/organisations", response_model=list[OrganisationPublic])
+def get_organisations(
+    *, 
+    session: Session = Depends(get_session), 
+    offset: int = 0, 
+    limit: int = Query(default=30, le=100)
+    ):
+    organisations = session.exec(select(Organisation)).all()
+    return organisations
+
+@app.get("/organisations/{org_id}", response_model=OrganisationPublicWithOwner)
+def get_organisation_by_id(*, session: Session = Depends(get_session), org_id: int):
+    organisation = session.get(Organisation, org_id)
+
+    if not organisation:
+        raise HTTPException(status_code=404, detail=f"Organisation not found.")
+    return organisation
+
+@app.get("/organisations/{org_id}/owner", response_model=UserPublic)
+def get_organisation_owner(*, session: Session = Depends(get_session), org_id: int):
+    organisation = session.get(Organisation, org_id)
+
+    if not organisation:
+        raise HTTPException(status_code=404, detail=f"Organisation not found.")
+    owner_id = organisation.owner_id
+
+    if not owner_id:
+        raise HTTPException(status_code=404, detail="Organisation owner not set.")
+
+    owner = session.get(User, owner_id)
+    if not owner:
+        raise HTTPException(status_code=404, detail="Owner not found.")
+
+    return owner
+
+@app.delete("/organisation/{org_id}")
+def delete_organisation_by_id(*, session: Session = Depends(get_session), org_id: int):
+    organisation = session.get(Organisation, org_id)
+
+    if not organisation:
+        raise HTTPException(status_code=404, detail="Organisation not found.")
+    
+    session.delete(organisation)
+    session.commit()
+
+    return {"Organisation: {org_id} - Deleted": True}
+
+@app.patch("/organisation/{org_id}", response_model=OrganisationPublic)
+def update_organisation(*, session: Session = Depends(get_session), org_id: int, organisation: OrganisationUpdate):
+    db_organisation = session.get(Organisation, org_id)
+
+    if not db_organisation:
+        raise HTTPException(status_code=404, detail="Organisation not found.")
+    
+    organisation_data = organisation.model_dump(exclude_unset=True)
+
+    db_organisation.sqlmodel_update(organisation_data)
+    session.add(db_organisation)
+    session.commit()
+    session.refresh(db_organisation)
+
+    return db_organisation
