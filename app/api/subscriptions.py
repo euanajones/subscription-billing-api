3f344e506705fa107f8fd04acd14e697383b3319
Euan Jones <euanajones@gmail.com>
2026-01-17 00:00:50 +0000

    Added create subscription endpoint

--- app/api/subscriptions.py
+++ app/api/subscriptions.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, Query, Depends
 from sqlmodel import Session, select
-from app.db.models import Subscription, SubscriptionPublic  
+from app.db.models import Subscription, SubscriptionPublic, SubscriptionCreate
 from main import app, get_session
 
 @app.get("/subscription", response_model=SubscriptionPublic)
@@ -20,3 +20,16 @@ def get_subscription_by_id(*, session: Session = Depends(get_session), sub_id: i
     if not subscription:
         HTTPException(status_code=404, detail="Subscription not found.")
     return subscription
+
+@app.post("/subscription/create", response_model=SubscriptionPublic)
+def create_subscription(
+    *, 
+    session: Session = Depends(get_session), 
+    subscription: SubscriptionCreate
+    ):
+    db_sub = Subscription.model_validate(subscription)
+    session.add(db_sub)
+    session.commit()
+    session.refresh(db_sub)
+    
+    return db_sub
